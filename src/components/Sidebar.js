fa11b3461ecce00f4e96e0d4f63671863ab8c412
Jiseok <chae0_@naver.com>
2023-11-20 21:19:19 +0900

    feat: sidebar, customhook

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { ReactComponent as PlusButton } from "../assets/images/plus.svg";
 import { ReactComponent as ChatIcon } from "../assets/images/textballon.svg";
+import useChatRoomNavigation from "../hook/useChatRoomNavigation";
+
 const Sidebar = () => {
   const [chatRooms, setChatRooms] = useState([]);
-
-  const handleNewChat = () => {
-    const newChatName = `Chatroom${chatRooms.length + 1}`;
-    setChatRooms((prevRooms) => [...prevRooms, newChatName]);
-  };
+  const { handleNewChat, handleChatRoomClick } = useChatRoomNavigation(chatRooms, setChatRooms);
 
   return (
     <SidebarContainer>
@@ -20,35 +17,31 @@ const Sidebar = () => {
       </NewbuttonContainer>
       <ChatList>
         {chatRooms.map((chat) => (
-          <StyledLink to={`/${chat}`} key={chat}>
-            <ChatItem>
-              <Chaticon />
-              {chat}
-            </ChatItem>
-          </StyledLink>
+          <ChatItem key={chat.id} onClick={() => handleChatRoomClick(chat.id)}>
+            <Chaticon />
+            {chat.name}
+          </ChatItem>
         ))}
       </ChatList>
     </SidebarContainer>
   );
 };
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: #e6e6e6;
-`;
+
 const NameContainer = styled.div`
-  /* text-align: center; */
-  padding-left: 1rem;
+  text-align: center;
   width: 17rem;
   height: 3rem;
   font-size: 2rem;
   font-weight: 700;
   margin-top: 0.5rem;
 `;
+
 const Chaticon = styled(ChatIcon)`
   width: 1rem;
   height: 1rem;
   margin-right: 0.5rem;
 `;
+
 const SidebarContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -68,6 +61,7 @@ const ChatList = styled.div`
   gap: 0.5rem;
   overflow-y: auto;
 `;
+
 const NewbuttonContainer = styled.button`
   display: flex;
   justify-content: center;
