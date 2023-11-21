605d28d4e52ae496601d79f9e0d36ba5c1c327dd
dhsin98 <shinjay417@gmail.com>
2023-11-21 18:02:03 +0900

    feat: main page 구조변경

---
+++ src/components/main/FileUpload.js
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import CustomBtnText from "../CustomBtn2";
+//파일 업로드 하는 부분
+
+const FileUpload = ({ onFileUpload }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    if (e.target.files.length > 0) {
+      e.preventDefault();
+      setSelectedFile(e.target.files[0]);
+      // onFileUpload(true);
+    }
+  };
+
+  const handleSendFile = async (e) => {
+    //여기서 이제 파일을 백으로 보냄
+    //FormData 객체에 담아서 보낸다-> 일단 예시 내용 담아둠
+    e.preventDefault();
+    if (selectedFile) {
+      //선텍된 파일이 있는 경우에만
+      // e.preventDefault();
+      onFileUpload(true);
+      // console.log(selectedFile);
+      // const formData = new FormData();
+      // formData.append("userId", userID);
+      // formData.append("file", selectedFile);
+
+      // axios({
+      //   method: "post",
+      //   url: "여기에 백 api 주소",
+      //   data: formData,
+      // })
+      //   .then((result) => {
+      //     console.log("요청성공");
+      //     console.log(result);
+      //   })
+      //   .catch((error) => {
+      //     console.log("요청실패");
+      //     console.log(error);
+      //   });
+    } else {
+      //선택된 파일이 없음
+      console.log("No file selected");
+    }
+  };
+  return (
+    <ChecklistItem>
+      <Form>
+        <FormTitle>Upload your file</FormTitle>
+        <FormParagraph>PDF file</FormParagraph>
+        <DropContainer htmlFor="file-input">
+          <DropTitle>Drop files here</DropTitle>
+          or
+          <FileInput
+            type="file"
+            accept=".pdf"
+            required
+            id="file-input"
+            onChange={handleFileChange}
+          />
+        </DropContainer>
+
+        <CustomBtnText
+          text="Send File"
+          textAfter="📨"
+          onClick={handleSendFile}
+        />
+      </Form>
+    </ChecklistItem>
+  );
+};
+
+//////
+const Form = styled.form`
+  background-color: #fff;
+  box-shadow: 0 10px 60px rgb(218, 229, 255);
+  border: 1px solid rgb(159, 159, 160);
+  border-radius: 20px;
+  padding: 2rem 0.7rem 0.7rem 0.7rem;
+  text-align: center;
+  font-size: 1.125rem;
+  max-width: 320px;
+  margin-top: 2rem;
+`;
+
+const FormTitle = styled.span`
+  color: #000000;
+  font-size: 1.8rem;
+  font-weight: 500;
+`;
+
+const FormParagraph = styled.p`
+  margin-top: 10px;
+  font-size: 0.9375rem;
+  color: rgb(105, 105, 105);
+`;
+
+const DropContainer = styled.label`
+  background-color: #fff;
+  position: relative;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  margin-top: 2.1875rem;
+  border-radius: 10px;
+  border: 2px dashed rgb(171, 202, 255);
+  color: #444;
+  cursor: pointer;
+  transition: background 0.2s ease-in-out, border 0.2s ease-in-out;
+
+  &:hover {
+    background: rgba(0, 140, 255, 0.164);
+    border-color: rgba(17, 17, 17, 0.616);
+  }
+`;
+
+const DropTitle = styled.span`
+  color: #444;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  transition: color 0.2s ease-in-out;
+
+  ${DropContainer}:hover & {
+    color: #222;
+  }
+`;
+
+const FileInput = styled.input`
+  width: 350px;
+  max-width: 100%;
+  color: #444;
+  padding: 2px;
+
+  background: #fff;
+  border-radius: 10px;
+  border: 1px solid rgba(8, 8, 8, 0.288);
+  &::-webkit-file-upload-button {
+    margin-right: 20px;
+    border: none;
+    background: #084cdf;
+    padding: 10px 20px;
+    border-radius: 10px;
+    color: #fff;
+    cursor: pointer;
+    transition: background 0.2s ease-in-out;
+    &:hover {
+      background: #0d45a5;
+    }
+  }
+`;
+const ChecklistItem = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  /* border: 1px solid rgb(159, 159, 160); */
+`;
+export default FileUpload;
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import Checklist from "../../components/Checklist";
+import PdfString from "../../components/PdfString";
+import Loader from "../loader/Loader";
+import FileUpload from "../../components/main/FileUpload";
+import Checklist from "../../components/main/Checklist";
+
+//백에서 String 받아온 부분,
+const StringReturned = ({ fileUploaded }) => {
+  const exampleString =
+    " PDF 가 string 으로 변환된 부분 - 예시 : Market Share (13 Aug. 202https://w3techs.com/technologies) Server-side programming languages for";
+  return fileUploaded && <PdfString string={exampleString} />;
+};
+
 const Main = () => {
+  const [fileUploaded, setFileUploaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFileUpload = () => {
+    setFileUploaded(true);
+  };
   return (
-    <MainContainer>
-      <Checklist />
-    </MainContainer>
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <MainContainer>
+          <FileUpload onFileUpload={handleFileUpload} />
+          <Checklist fileUploaded={fileUploaded} setIsLoading={setIsLoading} />
+        </MainContainer>
+      )}
+      <StringReturned fileUploaded={fileUploaded} />
+    </div>
   );
 };
 
-export default Main;
-
 const MainContainer = styled.div`
-  min-height: 100vh;
-  width: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: row;
+  gap: 10rem;
+  width: 60rem;
+  //객관식일때는 선지 개수 옵션이 늘어나니까, 화면 크기를 늘린다
+  height: ${(props) => (props.isMultipleChoice ? "50rem" : "45rem")};
+  padding: 10px 50px;
+  border-radius: 20px;
+  position: absolute;
+  top: 2rem;
+  left: 25rem;
+  /* border: 1px solid rgb(159, 159, 160); */
 `;
+
+export default Main;
