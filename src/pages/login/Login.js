fc4dfe30fbf2effff357f3f7b3f34e5d9e69fc31
dhsin98 <shinjay417@gmail.com>
2023-11-22 01:39:26 +0900

    style: kakao login 이미지 추가

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,37 +3,40 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import styled from "styled-components";
-
-
-const Login = ()=>
-{
-    const Rest_api_key='01edef910fa2573903206bddf92be765'
-    const redirect_uri = 'http://localhost:3000/'
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
-    const handleLogin = ()=>{
-        window.location.href = kakaoURL
-    }
-    return(
+import KakaoLoginButton from "../../assets/images/kakao_login_large_narrow.png";
+const Login = () => {
+  const Rest_api_key = "01edef910fa2573903206bddf92be765";
+  const redirect_uri = "http://localhost:3000/";
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
+  return (
     <>
-    <LoginContainer>
-    <KakaoLoginButton onClick={handleLogin}>카카오 로그인</KakaoLoginButton>
-    </LoginContainer>
+      <LoginContainer>
+        <img
+          src={KakaoLoginButton}
+          alt="카카오 로그인"
+          onClick={handleLogin}
+          style={{ cursor: "pointer" }}
+        />
+      </LoginContainer>
     </>
-    )
-}
+  );
+};
 export default Login;
-const KakaoLoginButton = styled.button`
-  background-color: #FEE500;
-  color: #391B1B;
-  font-size: 16px;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  &:hover {
-    background-color: #F7D600;
-  }
-`;
+// const KakaoLoginButton = styled.button`
+//   background-color: #fee500;
+//   color: #391b1b;
+//   font-size: 16px;
+//   padding: 10px 20px;
+//   border: none;
+//   border-radius: 5px;
+//   cursor: pointer;
+//   &:hover {
+//     background-color: #f7d600;
+//   }
+// `;
 
 const LoginContainer = styled.div`
   display: flex;
